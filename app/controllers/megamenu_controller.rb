34118d6242b1563fc5a171aac673ccbd23934e0b
Taylor Boren <borentaylor05@gmail.com>
2014-10-28 22:55:22 -0600

    Can now post from external site (Jive).  Need to add csrf token to osapi request.

--- app/controllers/megamenu_controller.rb
+++ app/controllers/megamenu_controller.rb
@@ -1,4 +1,9 @@
 class MegamenuController < ApplicationController
+
+	# Line below avoids need for csrf token verification.  Leaving this out will
+	# cause the Rails app to reject any POST request from external sites
+	# with Error: 422, Unprocessable_Entity
+	skip_before_action :verify_authenticity_token  
 	def click
 		respond_to do |format|
 			format.html 
